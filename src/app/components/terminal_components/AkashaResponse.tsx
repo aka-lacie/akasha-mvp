1fe6e9e8bc1710f60dee495488f53bbfcfedd33e
aka-lacie <freyaz495@gmail.com>
2023-10-17 16:29:03 -0700

    Add overflow scroll to answer bubble

--- src/app/components/terminal_components/AkashaResponse.tsx
+++ src/app/components/terminal_components/AkashaResponse.tsx
@@ -1,11 +1,53 @@
+import { useState, useEffect, useRef } from 'react';
 import ReconstructingText from './ReconstructingText';
 
 const AkashaResponse: React.FC<AkashaResponseProps> = ({ answer }) => {
+  const [showScrollDown, setShowScrollDown] = useState(false);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element = scrollRef.current;
+    if (element) {
+      setShowScrollDown(element.scrollHeight > element.clientHeight);
+    }
+  }, [answer]);
+
+  const handleScroll = () => {
+    const element = scrollRef.current;
+    if (element) {
+      const atBottom = element.scrollHeight - element.scrollTop === element.clientHeight;
+      setShowScrollDown(!atBottom);
+    }
+  };
+
+  const handleScrollDown = () => {
+    const element = scrollRef.current;
+    if (element) {
+      element.scrollTop = element.scrollHeight;
+      setShowScrollDown(false);
+    }
+  };
+
   return (
-    <div className='answerBubbleOpen min-w-[12rem]'>
-      <div className="text-sm text-center textClip overflow-hidden min-w-full max-w-prose">
+    <div className="answerBubbleOpen flex flex-col">
+      <div
+        className="text-sm text-center textClip min-w-[12rem] max-w-prose max-h-[40vh] overflow-y-scroll lg:max-h-none lg:overflow-visible"
+        ref={scrollRef}
+        onScroll={handleScroll}
+      >
         <ReconstructingText targetString={answer} />
       </div>
+      {showScrollDown && (
+        <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex self-center items-center justify-center">
+          <button
+            onClick={handleScrollDown}
+          >
+            <svg className="w-6 h-6 text-green-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+            </svg>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
